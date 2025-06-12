40568eec18cb712816f7ed5bfe53ba547e7c5726
Pavel Tarasov <paveltarasov121@gmail.com>
2025-06-13 00:25:18 +0300

    test dep

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => res.send("Express on Vercel"));
 app.use("/api", backgroundRoutes);
 
 export default app;
