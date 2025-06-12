4b72918158a129b7d1a407e255646b0e331b9e1a
Pavel Tarasov <paveltarasov121@gmail.com>
2025-06-13 00:46:11 +0300

    test vercel

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import backgroundRoutes from "./routes/background.routes";
 
 const app = express();
@@ -6,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, '..', 'public', 'index.html')));
 app.use("/api", backgroundRoutes);
 
 export default app;
